353611b4e931b8ef4825f8dbf4168a20ff590d35
Tasg39 <129655249+Tasg39@users.noreply.github.com>
2024-03-31 23:35:58 +0800

    新增
    新增第一章节UI，spine相关插件。
    完成UI搭建 开场动画

---
+++ Assets/Scripts/StartScene/UIPopPrologue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopPrologue : MonoBehaviour
+{
+    private Button btn_Computer;
+    private GameObject UIPopComputer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
