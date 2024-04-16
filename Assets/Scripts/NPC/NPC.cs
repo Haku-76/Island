cc864428c3a2fe0a6754883700015343ebaaad0b
YuYanxi <1836312914@qq.com>
2024-04-16 18:28:28 +0800

    完成NPC寻路

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("日程表")]
+    public ScheduleData_SO scheduleData;
+    private SortedSet<ScheduleDetails> scheduleSet;
+    private ScheduleDetails currentSchedule;
+
+    [Header("NPC 参数")]
+    public float move_Speed;
+    public bool interactable;
+
+    private float dir;
+
+    [Header("组件")]
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private Vector3 targetPosition;
+    private Vector3 startPosition;
+
+    private Stack<Vector3> targetPositions;
+    private Coroutine npcMoveRoutine;
+    private bool npcMove;
+    public bool isMoving;
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        targetPositions = new Stack<Vector3>();
+        scheduleSet = new SortedSet<ScheduleDetails>();
+        sprite.enabled = false;
+        foreach(var schedule in scheduleData.scheduleList)
+        {
+            scheduleSet.Add(schedule);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void InitNPC()
+    {
+        targetPosition = transform.position;
+    }
+
+    [ContextMenu("测试NPC 行为1")]
+    public void Test()
+    {
+        OnTimeUpdateEvent(1, TimeQuantum.DayTime);
+    }
+
+
+    private void OnTimeUpdateEvent(int day, TimeQuantum timeQuantum)
+    {
+        int time = day * 100 + (int)timeQuantum;
+
+        ScheduleDetails matchSchedule = null;
+        foreach(var schedule in scheduleSet)
+        {
+            if(schedule.Time == time)
+            {
+                matchSchedule = schedule;
+            }
+            else if(schedule.Time > time)
+            {
+                break;
+            }
+        }
+
+        if(matchSchedule != null)
+        {
+            BuildPath(matchSchedule);
+        }
+    }
+
+    private void Movement()
+    {
+        if(!npcMove)
+        {
+            if(targetPositions.Count > 0)
+            {
+                targetPosition = targetPositions.Pop();
+                
+                StartCoroutine(BurnNPC(() => {StartCoroutine(MoveRoutine(targetPosition, move_Speed));} ));
+            }
+            
+        }
+    }
+
+    private IEnumerator BurnNPC(System.Action onBurnComplete)
+    {
+        transform.position = startPosition;
+        sprite.enabled = true;
+        Color startColor = Color.clear;
+        Color targetColor = Color.white;
+        float burnDuration = 1.0f;
+        float elapsedTime = 0f;
+        while(elapsedTime < burnDuration)
+        {
+            sprite.color = Color.Lerp(startColor, targetColor, elapsedTime/burnDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = targetColor;
+        onBurnComplete?.Invoke();
+    }
+
+    private void MoveToTargetPosition(Vector3 target, float speed)
+    {
+        npcMoveRoutine = StartCoroutine(MoveRoutine(target, speed));
+    }
+
+    private IEnumerator MoveRoutine(Vector3 targetPos, float speed)
+    {
+        npcMove = true;
+        interactable = false;
+        Vector3 startPos = transform.position;
+        float distance = Vector3.Distance(startPos, targetPos);
+        float journeyTime = distance / speed;
+        float elapsedTime = 0f;
+
+        dir = targetPos.x - transform.position.x;
+
+        while(elapsedTime < journeyTime)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / journeyTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.position = targetPos;
+        interactable = true;
+        npcMove = false;
+    }
+
+    public void BuildPath(ScheduleDetails schedule)
+    {
+        targetPositions.Clear();
+        currentSchedule = schedule;
+
+        targetPositions.Push(currentSchedule.targetPosition);
+        startPosition = schedule.burnPosition;
+    }
+
+    public void SwitchAnimation()
+    {
+        isMoving = transform.position != targetPosition;
+        anim.SetBool("IsMoving", isMoving);
+
+        if(isMoving)
+        {
+            anim.SetFloat("Dir", dir);
+        }
+        else
+        {
+            anim.SetBool("Exit", false);
+        }
+    }
+}
--- Assets/Scripts/NPC/ScheduleDetails.cs
+++ Assets/Scripts/NPC/ScheduleDetails.cs
@@ -1,8 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScheduleDetails
+[Serializable]
+public class ScheduleDetails : IComparable<ScheduleDetails>
 {
     public int day;
+    public TimeQuantum time;
+
+    public int priority;
+
+    public Vector3 burnPosition;
+    public Vector3 targetPosition;
+
+    //TODO:对话匣
+
+    public bool interactable;
+
+    
+
+    public ScheduleDetails(int day, TimeQuantum time, int priority, Vector3 burnPosition, Vector3 targetPosition, bool interactable)
+    {
+        this.day = day;
+        this.time = time;
+        this.priority = priority;
+        this.burnPosition = burnPosition;
+        this.targetPosition = targetPosition;
+        this.interactable = interactable;
+    }
+    public int Time => day * 100 + (int)time;
+    public int CompareTo(ScheduleDetails other)
+    {
+        if(Time == other.Time)
+        {
+            if(priority > other.priority)
+                return 1;
+            else
+                return -1;
+        }
+        else if(Time > other.Time)
+            return 1;
+        else if(Time < other.Time)
+            return -1;
+        
+        return 0;
+    }
 }
