17b63a97c47301692d53bbf1dc18e46b27410ccf
KyleKK04 <Kyle20040930@Gmail.com>
2024-05-08 13:15:57 +0800

    新手教程完毕
    目前问题：镜头应当平滑移动至上下楼箭头和信箱，目测原因是因为两个相机机位重叠了。
    需要整合：title：MakeWineGuide在点击GuideMan上，我在GuideManController里继承了NPC，但是不知后续如何操作
    title：TimeGuide，是对时间的教程
    新手教程文案大家可帮忙调整一下，以及GuideMan的模型后面记得换一下。
    辛苦各位了

--- Assets/Scripts/Transition/CameraStandAndTrigger.cs
+++ Assets/Scripts/Transition/CameraStandAndTrigger.cs
@@ -18,15 +18,16 @@ public class CameraStandAndTrigger : MonoBehaviour
         Debug.Log($"Enter{this.name}Area");
         if(other.gameObject.CompareTag("Player"))
         {
-            TransitionManager.Instance.SetCamera(cameraStand, isFollowPlayer);
+            TransitionManager.Instance.SetCameraWithTransition(cameraStand, isFollowPlayer);
         }
 
         if (this.gameObject.name == "Trigger_Upstairs" && isUpstairsTriggered == false)
         {
             isUpstairsTriggered = true;
-            TransitionManager.Instance.SetCamera(cameraStand,isFollowPlayer);
+            TransitionManager.Instance.SetCameraWithTransition(cameraStand,isFollowPlayer);
             DialogueManager.Instance.StartDialogue("UpStairsGuide");
             Destroy(this.gameObject);
+            TransitionManager.Instance.ReturnPreCamera();
         }
         if (this.gameObject.name == "Trigger_Outside_Details_1" && isOutsideTriggered1 == false)
         {
@@ -37,14 +38,18 @@ public class CameraStandAndTrigger : MonoBehaviour
         if (this.gameObject.name == "Trigger_Bed" && isSleepTriggered == false)
         {
             isSleepTriggered = true;
+            TransitionManager.Instance.SetCameraWithTransition(cameraStand,isFollowPlayer);
             DialogueManager.Instance.StartDialogue("SleepGuide");
             Destroy(this.gameObject);
+            TransitionManager.Instance.ReturnPreCamera();
         }
 
         if (this.gameObject.name == "Trigger_Outside_Details_2" && isOutsideTriggered2 == false)
         {
             isOutsideTriggered2 = true;
+            TransitionManager.Instance.SetCameraWithTransition(cameraStand,isFollowPlayer);
             DialogueManager.Instance.StartDialogue("MailboxGuide");
+            TransitionManager.Instance.ReturnPreCamera();
         }
     }
 }
